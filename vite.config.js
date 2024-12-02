14452261d554a0e90bb623af631ff4e2e26c1eb2
VincentMugondora <vinmugondora@gmail.com>
2024-12-02 10:06:04 +0200

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -17,4 +17,24 @@ export default defineConfig({
       }
     },
   },
-}); 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor libraries into their own chunk
+          if (id.includes('node_modules')) {
+            return 'vendor'; // All node_modules will go into a separate chunk
+          }
+          // You can add more conditions to create other chunks based on your needs
+          if (id.includes('src/components')) {
+            return 'components'; // Separate components into their own chunk
+          }
+          if (id.includes('src/utils')) {
+            return 'utils'; 
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600 
+  }
+});
