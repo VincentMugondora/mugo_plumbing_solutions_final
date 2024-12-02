1cbe970be9ec728947abc40bf5f12e6c746183d8
VincentMugondora <vinmugondora@gmail.com>
2024-12-02 12:44:36 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/auth": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
-      '/auth': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split vendor libraries into their own chunk
-          if (id.includes('node_modules')) {
-            return 'vendor'; // All node_modules will go into a separate chunk
-          }
-          // You can add more conditions to create other chunks based on your needs
-          if (id.includes('src/components')) {
-            return 'components'; // Separate components into their own chunk
-          }
-          if (id.includes('src/utils')) {
-            return 'utils'; 
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 600 
-  }
+  };
 });
